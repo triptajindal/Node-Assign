7e2b5d8b6dbb7109d531b13b0367e22262c9f222
triptajindal <tripta.jindal87@gmail.com>
2019-03-13 10:30:04 +0530

    my node assignment

---
+++ modules.js
@@ -0,0 +1,7 @@
+var counter= function(a,b){
+  console.log( "the addition: "+(a+b));
+  console.log( "the subtraction: "+(a-b));
+  console.log( "the multiplication: "+(a*b));
+  console.log( "the division: "+(a/b));
+}
+module.exports.counter=counter
---
+++ nodeAssign.js
@@ -0,0 +1,3 @@
+var abc= require('./modules')
+
+abc.counter(2,3)
